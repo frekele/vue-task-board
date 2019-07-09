492f3bc2b2888e2cbb63eb3daaa98678aa6a29ef
Leandro Kersting de Freitas <leandro@frekele.org>
2019-07-09 04:04:15 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,91 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
+        <Header title="Task Board"
+                :hideToggle="!user"
+                :hideUserDropdown="!user"/>
+        <Menu v-if="user"/>
+        <Loading v-if="validatingToken"/>
+        <Content v-else/>
+        <Footer/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+    import axios from "axios"
+    import {baseApiUrl, userKey} from "@/global"
+    import {mapState} from "vuex"
+    import Header from "@/components/template/Header"
+    import Menu from "@/components/template/Menu"
+    import Content from "@/components/template/Content"
+    import Footer from "@/components/template/Footer"
+    import Loading from "@/components/template/Loading"
+
+    export default {
+        name: "App",
+        components: {Header, Menu, Content, Footer, Loading},
+        computed: mapState(['isMenuVisible', 'user']),
+        data: function () {
+            return {
+                validatingToken: true
+            }
+        },
+        methods: {
+            async validateToken() {
+                this.validatingToken = true
+
+                const json = localStorage.getItem(userKey)
+                const userData = JSON.parse(json)
+                this.$store.commit('setUser', null)
+
+                if (!userData) {
+                    this.validatingToken = false
+                    this.$router.push({name: 'auth'})
+                    return
+                }
+
+                const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+                if (res.data) {
+                    this.$store.commit('setUser', userData)
+
+                    if (this.$mq === 'xs' || this.$mq === 'sm') {
+                        this.$store.commit('toggleMenu', false)
+                    }
+                } else {
+                    localStorage.removeItem(userKey)
+                    this.$router.push({name: 'auth'})
+                }
+
+                this.validatingToken = false
+            }
+        },
+        created() {
+            this.validateToken()
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+    * {
+        font-family: "Lato", sans-serif;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    body {
+        margin: 0;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #app {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 60px 1fr 40px;
+        grid-template-columns: 300px 1fr;
+        grid-template-areas: "header header" "menu content" "menu footer";
+    }
+
+    #app.hide-menu {
+        grid-template-areas: "header header" "content content" "footer footer";
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
+import 'font-awesome/css/font-awesome.css'
 import './style.css'
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+
+import App from './App'
+
+import './config/bootstrap'
+import './config/msgs'
+import './config/axios'
+import './config/mq'
+
+import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    store,
+    router,
+    render: h => h(App)
 }).$mount('#app')
